9b58fdbf7ba05ea17d6e0a52222d845caab555a8
李星宇 <1723980130@qq.com>
2026-03-23 15:07:25 +0800

    [描述]{设置问题目录范围} [语言]{java} [类型]{代码模板添加文件夹类型} [解决方案]{无} [测试建议]{无} [相关链接]{无}

---
+++ Leetcode/src/generator/QuestionDir.java
@@ -0,0 +1,13 @@
+package generator;
+
+public interface QuestionDir {
+
+    public static final String SIMPLE = "simple";
+
+
+    public static final String MEDIUM = "medium";
+
+
+    public static final String DIFFICULTY = "difficulty";
+
+}
